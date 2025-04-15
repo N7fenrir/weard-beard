ddd3bc113d3672c9dbc734d5e279abc28559090e
Vighnesh Bheed <vighneshbheed@outlook.com>
2025-04-15 19:58:06 +0200

    updated service 1

--- service1/src/main.rs
+++ service1/src/main.rs
@@ -1,5 +1,19 @@
 use std::error::Error;
+use tokio::sync::mpsc::{self};
+use tokio::time::Instant;
+use sensors::print_readings;
+
 mod config;
+mod sensors;
+
+async fn wait_for_shutdown_signal() {
+    println!("Service running. Press Ctrl+C to stop.");
+    if let Err(e) = tokio::signal::ctrl_c().await {
+        eprintln!("Failed to listen for Ctrl+C signal: {}", e);
+    } else {
+        println!("\nCtrl+C received. Initiating shutdown...");
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -7,11 +21,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let sensors_to_simulate = config::define_sensors();
 
-    println!("=================================================");
-    for sensor in sensors_to_simulate {
+    println!("==================== ::Sensors:: ============================ ");
+    for sensor in &sensors_to_simulate {
         println!("Sensor Available {:?} ", sensor);
     }
-    println!("=================================================");
+    println!(" ============================ :::: ============================ ");
+
+    let (output_sender, output_receiver) = mpsc::channel::<sensors::SensorReading>(100); // Buffer size 100
+
+    let simulation_start_time = Instant::now();
+
+    let _sensor_task_handles =
+        sensors::spawn_sensor_tasks(sensors_to_simulate, output_sender, simulation_start_time);
+
+    let _printer_task_handle = tokio::spawn(async move {
+        print_readings(output_receiver).await;
+    });
+
+    wait_for_shutdown_signal().await;
 
     println!("--- Sensor Simulator Service Shutting Down ---");
 
